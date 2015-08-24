63dc63488be4ff4ee0d05c29ce0b60db9bf02eb0
zwl <zwllwz.z@gmail.com>
2015-08-23 23:37:54 -0400

    add item detail router

--- src/app.js
+++ src/app.js
@@ -8,28 +8,44 @@ angular.module('100das', [
   '100das.category'
   ])
 .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  var home = {
+    name: 'home',
+    url: '/home',
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl'
+  }
+  var category = {
+    name: 'category',
+    url: '/category',
+    templateUrl: 'category/category.html',
+    controller: 'CategoryCtrl'
+  }
+  var about = {
+    name: 'about',
+    url:'/about',
+    templateUrl:'about/about.html',
+    controller: 'AboutCtrl'
+  }
+
+  var itemList = {
+    name: 'itemList',
+    url: '/itemList',
+    templateUrl: 'item/item.html',
+    controller: 'ItemCtrl'
+  }
+
+  var itemDetail = {
+    name: 'itemDetail',
+    url: '/itemDetail',
+    templateUrl: 'item/itemDetail.html',
+    controller: 'ItemDetailCtrl'
+  }
   $urlRouterProvider.otherwise("/home");
   $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'home/home.html',
-      controller: 'HomeCtrl'
-    })
-    .state('category', {
-      url: '/category',
-      templateUrl: 'category/category.html',
-      controller: 'CategoryCtrl'
-    })
-    .state('category.itemList', {
-      url: '/itemList',
-      templateUrl: 'item/item.html',
-      controller: 'ItemCtrl'
-    })
-    .state('about', {
-      url:'/about',
-      templateUrl:'about/about.html',
-      controller: 'AboutCtrl'
-    });
+    .state(home)
+    .state(category)
+    .state(itemList)
+    .state(about);
 }])
 .controller('MainCtrl', ['$scope', '$state', function ($scope, $state) {
   $scope.name = 'world';
--- src/category/categoryCtrl.js
+++ src/category/categoryCtrl.js
@@ -1,4 +1,7 @@
 angular.module('100das.category')
-.controller('CategoryCtrl', ['$scope', function ($scope) {
+.controller('CategoryCtrl', ['$scope', '$state', function ($scope, $state) {
   $scope.categories = ['category1', 'category2', 'category3', 'category4'];
+  $scope.getItems = function () {
+  	$state.go('itemList');
+  }
 }]);
