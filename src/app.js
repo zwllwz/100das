594ad312066089ca03406f51b3fb3bc5f447f6d2
Lara Zhu <lara.zhu@gm.com>
2015-08-31 13:47:38 -0400

    reactjs app

--- src/app.js
+++ src/app.js
@@ -1,59 +1,9 @@
-angular.module('100das', [
-  'ui.router',
-  '100das.header',
-  '100das.home',
-  '100das.item',
-  '100das.about',
-  '100das.category'
-  ])
-.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-  var tab = {
-    name: 'tab',
-    url: '/tab',
-    templateUrl: 'header/header.html',
-    controller: 'HeaderCtrl'
-  };
-  var home = {
-    name: 'tab.home',
-    url: '/home',
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  };
-  var category = {
-    name: 'tab.category',
-    url: '/category',
-    templateUrl: 'category/category.html',
-    controller: 'CategoryCtrl'
-  };
-  var about = {
-    name: 'tab.about',
-    url:'/about',
-    templateUrl:'about/about.html',
-    controller: 'AboutCtrl'
-  };
-
-  var itemList = {
-    name: 'itemList',
-    url: '/itemList',
-    templateUrl: 'item/item.html',
-    controller: 'ItemCtrl'
-  };
-
-  var itemDetail = {
-    name: 'itemDetail',
-    url: '/itemDetail',
-    templateUrl: 'item/itemDetail.html',
-    controller: 'ItemDetailCtrl'
-  };
-  $urlRouterProvider.otherwise('/tab');
-  $stateProvider
-    .state(tab)
-    .state(home)
-    .state(category)
-    .state(itemList)
-    .state(about);
-}])
-.controller('MainCtrl', ['$scope', '$state', function ($scope, $state) {
-  $scope.name = 'world';
-
-}]);
+var CommentBox = Reat.creatClass({
+  render: function () {
+    return (
+      <div className="commentBox">
+        heelo. wokld! iam a CommentBox
+      </div>
+    )
+  }
+});
